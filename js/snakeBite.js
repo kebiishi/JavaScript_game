ce7f16012c579342fa9e15db105ff778ec25791a
genki <yagozogennki@gmail.com>
2018-06-10 18:39:00 +0900

    蛇と餌の初期表示に成功

--- js/snakeBite.js
+++ js/snakeBite.js
@@ -1,19 +1,30 @@
-var width;
-var height;
-var areaNum = 20;
+var ctx;
+var areaX;
+var areaY;
+var areaWidth = 40;
 var snake = [], food = [];
 var keyCode = 0;
+
+// Pointクラス
+function Point(x, y) {
+	this.x = x;
+	this.y = y;
+}
 
 // 初期化関数
 function init() {
 	var canvas = document.getElementById("field");
-	width = canva.width / areaNum;
-	height = canva.height / areaNum;
 	ctx = canvas.getContext("2d");
-	ctx.font = "20px sans-serif";
+	ctx.font = "40px sans-serif";
+
+	// x方向、y方向のマスの数
+	areaX = canvas.width / areaWidth;
+	areaY = canvas.height / areaWidth;
+	console.log(canvas.width + " " + canvas.height);
+	console.log(areaX + " " + areaY);
 
 	// 蛇の初期化
-	snake.push(new Point(width / 2., height / 2.));
+	snake.push(new Point(areaX / 2, areaY / 2));
 
 	// 餌の初期化
 	for (var i = 0; i < 10; i++) {
@@ -25,11 +36,14 @@ function init() {
 
 // 餌を追加
 function addFood() {
-
+	var x = Math.floor(areaX * Math.random());
+	var y = Math.floor(areaY * Math.random());
+	var foodPos = new Point(x, y);
+	return foodPos;
 }
 
 function tick() {
-
+	paint();
 }
 
 function keydown() {
@@ -38,5 +52,14 @@ function keydown() {
 
 // 描画
 function paint() {
+	ctx.clearRect(0, 0, areaX * areaWidth, areaY * areaWidth);
+	ctx.fillStyle = "green";
+	snake.forEach(function (i) {
+		ctx.fillText("●", i.x * areaWidth, i.y * areaWidth);
+	});
+	ctx.fillStyle = "yellow";
+	food.forEach(function (i) {
+		ctx.fillText("*", i.x * areaWidth, i.y * areaWidth);
+	});
 
 }
